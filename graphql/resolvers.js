f7f2e7055aa7c1c757e313a2ff75f5382a03806a
Natalie-Cardot <nataliecardot@gmail.com>
2020-06-25 17:31:56 -0700

    Use destructuring

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,10 @@
 // Define logic that will be executed for incoming queries
 module.exports = {
   // args: input (args wil be an object containing all the arguments passed to function, a userInput field with email, name, password)
-  createUser(args, req) {
+  // Here, using destructuring to get userInput out of args object
+  createUser({ userInput }, req) {
     // .userInput because named the field that way in schema
-    const email = args.userInput.email;
+    // const email = args.userInput.email;
+    const email = userInput.email;
   },
 };
