15f8e47e99ccf7485913a40349aae060d77a216c
Natalie-Cardot <nataliecardot@gmail.com>
2020-06-25 17:30:10 -0700

    Add resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,2 +1,8 @@
 // Define logic that will be executed for incoming queries
-module.exports = {};
+module.exports = {
+  // args: input (args wil be an object containing all the arguments passed to function, a userInput field with email, name, password)
+  createUser(args, req) {
+    // .userInput because named the field that way in schema
+    const email = args.userInput.email;
+  },
+};
