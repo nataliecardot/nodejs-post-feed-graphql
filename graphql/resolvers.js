14325533e48fc0a18028219413abe2b39f5ddbcb
Natalie-Cardot <nataliecardot@gmail.com>
2020-06-25 17:05:20 -0700

    Remove resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,2 @@
 // Define logic that will be executed for incoming queries
-module.exports = {
-  hello() {
-    return {
-      // On the server GraphQL filters out just the data requested by client
-      text: 'Hello world!',
-      views: 1245,
-    };
-  },
-};
+module.exports = {};
