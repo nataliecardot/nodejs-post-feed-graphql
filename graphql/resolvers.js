dbc477331857a2cb987950e3a1f2225fba98d36a
Natalie-Cardot <nataliecardot@gmail.com>
2020-06-25 19:54:10 -0700

    Use destructuring

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -4,23 +4,21 @@ const User = require('../models/user');
 
 // Define logic that will be executed for incoming queries
 module.exports = {
+  // async createUser({ userInput }, req) { // Refactored to use ES6 concise method syntax?
   // args: input (args wil be an object containing all the arguments passed to function, a userInput field with email, name, password)
-  // Here, using destructuring to get userInput out of args object
-  // async createUser({ userInput }, req) { // Refactored to use ES6 concise method syntax
-  createUser: async function ({ userInput }, req) {
+  // Pulling out email and name from userInput, which is pulled out of args. So don't need args.userInput.email, etc.
+  createUser: async function ({ userInput: { email, name, password } }, req) {
     // .userInput because named the field that way in schema
-    // const email = args.userInput.email;
-    // const email = userInput.email; // With destructuring
     // If not using async/await, need to return User.findOne() with then chained on; if you don't return promise in resolver, GraphQL will not wait for it to resolve. But when using async/await, it's returned behind the scenes
-    const existingUser = await User.findOne({ email: userInput.email });
+    const existingUser = await User.findOne({ email });
     if (existingUser) {
       const error = new Error('User exists already.');
       throw error;
     }
-    const hashedPw = await bcrypt.hash(userInput.password, 12); // 12 salting rounds
+    const hashedPw = await bcrypt.hash(password, 12); // 12 salting rounds
     const user = new User({
-      email: userInput.email,
-      name: userInput.name,
+      email,
+      name,
       password: hashedPw,
     });
   },
