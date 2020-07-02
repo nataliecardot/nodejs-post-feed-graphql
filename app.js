894df17b200d2642b7b3257155e084dd58dc7b03
Natalie-Cardot <nataliecardot@gmail.com>
2020-07-02 15:05:29 -0700

    Change connection string

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 
 const MONGODB_URI =
   // process object is globally available in Node app; part of Node core runtime. env property contains all environment variables known by process object. Using dotenv to store environment variables. It loads environment variables from .env file into process.env (see https://www.youtube.com/watch?v=17UVejOw3zA)
-  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-4yuid.mongodb.net/${process.env.MONGO_DATABASE_NAME}/?retryWrites=true`;
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-4yuid.mongodb.net/${process.env.MONGO_DATABASE_NAME}?retryWrites=true`;
 
 const app = express();
 
