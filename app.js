607b33dab6b64a7410481d539fda95194bbe6d7d
Natalie-Cardot <nataliecardot@gmail.com>
2020-07-06 17:32:09 -0700

    Move clearImage function into util folder

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,6 +10,7 @@ const { uuid } = require('uuidv4');
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 // Tells server to load anything in .env file into an environment variable.
 require('dotenv').config();
@@ -132,8 +132,3 @@ mongoose
     app.listen(8080);
   })
   .catch((err) => console.log(err));
-
-const clearImage = (filePath) => {
-  filePath = path.join(__dirname, '.', filePath);
-  fs.unlink(filePath, (err) => console.log(err));
-};
